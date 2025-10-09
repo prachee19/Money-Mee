7ff04cb94c2a4c06ee884128ac90da8ab4502087
prachee19 <166029923+prachee19@users.noreply.github.com>
2025-10-10 00:16:38 +0530

    Update SecurityConfig.java FOR / in api point

--- src/main/java/com/me/moneymanager/config/SecurityConfig.java
+++ src/main/java/com/me/moneymanager/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(
                         auth -> auth
                                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                                .requestMatchers("/status", "/health", "/register","/login", "api/v1.0/login", "/activate",
+                                .requestMatchers("/status", "/health", "/register","/login", "/api/v1.0/login", "/activate",
                                         "/api/v1.0/dashboard", "/filter", "/api/v1.0/incomes", "/api/v1.0/expenses")
                                 .permitAll()
                                 .anyRequest().authenticated())
@@ -53,7 +53,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("https://moneymee.netlify.app"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
         configuration.setExposedHeaders(List.of("Content-Disposition"));
