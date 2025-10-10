2558693fb390a6f13fa64d1b17f09780c83a6b08
prachee19 <prachigawande190105@gmail.com>
2025-10-10 16:17:05 +0530

    changes for request mapping /api/v1.0

--- src/main/java/com/me/moneymanager/controller/ProfileController.java
+++ src/main/java/com/me/moneymanager/controller/ProfileController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ import com.me.moneymanager.dto.ProfileDTO;
 import com.me.moneymanager.service.ProfileService;
 
 @RestController
+@RequestMapping("/api/v1.0")
 @RequiredArgsConstructor
 public class ProfileController {
 
